0b4eef12f360eda19332733aae285a00997fce69
Nathan Ramaker <nathan.ramaker@helpsystems.com>
2018-02-22 13:16:40 -0600

    task1 done

--- clustering.py
+++ clustering.py
@@ -3,41 +3,34 @@ import numpy as np
 import pandas as pd
 from scipy.cluster.hierarchy import dendrogram, linkage
 from matplotlib import pyplot as plt
+from scipy.spatial import distance
+from scipy.spatial.distance import pdist
+from sklearn.metrics import euclidean_distances
 
 def load_data(file):
     print("Loading samples from file: {}".format(file))
-    data = pd.read_csv(file, header=None)
+    data = pd.read_csv(file)
     
-    print("Full data set is {} lines.".format(len(data)))
+    # print("Full data set is {} lines.".format(len(data)))
 
-    return data.iloc[1:], data.iloc[0]
+    countries = data['Country'].as_matrix()
+    vectors = data.T[1:].T.as_matrix()
+    return vectors, countries
 
 def cluster_and_plot(vectors, labels, description, link_type='average',):
-    X = [[i] for i in vectors]
-    Z = linkage(X, method=link_type, metric='euclidean')
-    fig = plt.figure(figsize=(10, 10))
-    dn = dendrogram(Z)
-    fig.title = description
-    plt.xlabel(countries)
+    #X = [[i] for i in vectors]
+    X = euclidean_distances(vectors)
+    Z = linkage(X, method=link_type)
+    fig = plt.figure(figsize=(12, 8))
+    dn = dendrogram(Z, labels=labels)
+    plt.title(description)
     plt.show()
-
-# def produce_clusters(data):
-#     return data
-
-# def plot_dendogram(data):
-#     return data
     
-#main entry
-if __name__ == "__main__":
-    print(" ##### AML HW4 Clusterererer  ##### ")
+def task1():
     print("")
+    print("### Task1")
     print("### Loading Data ...")
-    data, labels = load_data('data.csv')
-    countries = data[0]
-    vectors = data.T[1:].T
-
-    # print("vectors {}".format(vectors))
-    # print("countries {}".format(countries))
+    vectors, countries = load_data('data.csv')
  
     print("### Producing Clusters with Single Link")
     cluster_and_plot(vectors, countries, link_type='single', description="Single Link")
@@ -47,3 +40,16 @@ if __name__ == "__main__":
 
     print("### Producing Clusters with Complete Link")
     cluster_and_plot(vectors, countries, link_type='complete', description="Complete Link")
+
+def task2():
+    pass
+
+def task3():
+    pass
+
+#main entry
+if __name__ == "__main__":
+    print(" ##### AML HW4 Clusterererer  ##### ")
+    task1()
+    task2()
+    task3()
