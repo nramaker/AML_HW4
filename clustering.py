73136fa3259e219a90fbdaa578e46de8100854d5
Nathan Ramaker <nathan.ramaker@helpsystems.com>
2018-03-02 13:19:24 -0600

    splitting attempt

--- clustering.py
+++ clustering.py
@@ -6,14 +6,15 @@ import os
 from scipy.cluster.hierarchy import dendrogram, linkage
 from sklearn.metrics import euclidean_distances
 from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split
 
 from matplotlib import pyplot as plt
+
+seed = 42
 
 def load_data(file):
     print("Loading samples from file: {}".format(file))
     data = pd.read_csv(file)
-    
-    # print("Full data set is {} lines.".format(len(data)))
 
     countries = data['Country'].as_matrix()
     vectors = data.T[1:].T.as_matrix()
@@ -53,10 +54,6 @@ def k_means(k=3):
         kmeans = KMeans(n_clusters=i, random_state=0).fit(vectors)
         ks.append(i)
         distances.append(kmeans.inertia_/len(countries))
-        # df = pd.DataFrame()
-        # df['countries'] = countries
-        # df['labels'] = kmeans.labels_
-        # print(df)
     
     plt.figure(1)
     
@@ -68,9 +65,20 @@ def k_means(k=3):
     plt.show()
 
 def task3():
-    #TODO load data
-    files = load_and_join_data('./HMP_Dataset')
-    #TODO split data into test and training
+    labels = list_labels()
+    print("labels {}".format(labels))
+    dirs = list_dirs("./HMP_Dataset", labels)
+    all_data = []
+    for directory in dirs:
+        data = load_and_join_data(directory)
+        all_data.append([data])
+    #split data into test and training
+    train_X, test_X, train_Y, test_Y = test_train_split(data, labels, .20)
+    print("train_X.shape {}".format(np.asarray(train_X).shape))
+    print("train[0] {}".format(train[0]))
+    print("test_X.shape {}".format(np.asarray(test_X).shape))
+    print("train_Y.shape {}".format(np.asarray(train_Y).shape))
+    print("test_Y.shape {}".format(np.asarray(test_Y).shape))
     #TODO build dictionary
     #       cut signals into fixed size
     #       clustering with kmeans
@@ -81,7 +89,39 @@ def task3():
     
     pass
 
+def test_train_split(data, labels, percent_test=.20):
+    train_data = []
+    train_labels = []
+    test_data = []
+    test_labels = []
+    for i in range(0, len(labels)):
+        print("Splitting {} data into test/train, test split = {}".format(labels[i], percent_test))
+        #construct labels
+        X=data[i]
+        y = [[]] * len(X)
+        y[0].append(labels[i])
+        #split into training and test
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=percent_test, random_state=seed)
+        train_data.append(X_train)
+        test_data.append(X_test)
+        train_labels.append(y_train)
+        test_labels.append(y_test) 
+
+    return train_data, test_data, train_labels, test_labels
+
+def list_labels():
+    labels = ["Brush_teeth","Climb_stairs","Comb_hair","Descend_stairs","Drink_glass","Eat_meat","Eat_soup","Getup_bed","Liedown_bed",
+    "Pour_water","Sitdown_chair","Standup_chair","Use_telephone","Walk"]
+    return labels
+
+def list_dirs(parent_dir, labels):
+    dirs = []
+    for label in labels:
+        dirs.append(parent_dir+"/"+label)
+    return dirs
+
 def load_and_join_data(parent_dir):
+    print("Loading data from {}".format(parent_dir))
     #get all files in the directory, excluding _MODEL directories
     x = [os.path.join(r,file) for r,d,f in os.walk(parent_dir) for file in f]
     # print(x)
@@ -102,20 +142,14 @@ def load_and_join_data(parent_dir):
             trimmed.append(path)
     # print(trimmed)
     data = []
-    # print("File {}".format(trimmed[0]))
-    # my_data = genfromtxt(trimmed[0], delimiter=' ')
-    #df = pd.read_csv(trimmed[0], header=None)
-    # data.append(my_data)
-    print(data)
+    # print(data)
     for t in trimmed:
         my_data = genfromtxt(t, delimiter=' ')
         for line in my_data:
             data.append(line)
-        # print("my_data.shape {}".format(np.asarray(my_data).shape))
-        #data.append(my_data)
 
     print("data shape {}".format(np.asarray(data).shape))
-    print("data[0] {}".format(data[0]))
+    # print("data[0] {}".format(data[0]))
     return data
 
 
